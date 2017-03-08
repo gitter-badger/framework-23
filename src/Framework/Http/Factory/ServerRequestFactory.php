f58c39f5aba96b8d21035ae55f19f9490b743a06
Dimitar Dimitrov <daghostman.dd@gmail.com>
2017-03-08 14:32:08 +0200

    Handle cases where HTTP_METHOD is not set

--- src/Framework/Http/Factory/ServerRequestFactory.php
+++ src/Framework/Http/Factory/ServerRequestFactory.php
@@ -6,21 +6,27 @@ use Onion\Framework\Dependency\Interfaces\FactoryInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
+/**
+ * Class ServerRequestFactory
+ *
+ * @package Onion\Framework\Http\Factory
+ */
 final class ServerRequestFactory implements FactoryInterface
 {
-
     /**
      * Method that handles the construction of the object
      *
      * @param ContainerInterface $container DI Container
      *
      * @return \Psr\Http\Message\ServerRequestInterface
+     * @throws \InvalidArgumentException
      */
     public function build(ContainerInterface $container): ServerRequestInterface
     {
         $server = filter_input_array(INPUT_SERVER) ?? null;
         $payload = null;
-        if (in_array($server['HTTP_METHOD'], ['POST', 'PUT', 'PATCH'], true) && isset($server['HTTP_CONTENT_TYPE'])) {
+        $method = $server['HTTP_METHOD'] ?? 'GET';
+        if (isset($server['HTTP_CONTENT_TYPE']) && in_array($method, ['POST', 'PUT', 'PATCH'], true)) {
             $payload = filter_input_array(INPUT_POST);
             if ($server['HTTP_CONTENT_TYPE'] === 'application/json') {
                 $payload = json_decode(file_get_contents('php://input'), true);
