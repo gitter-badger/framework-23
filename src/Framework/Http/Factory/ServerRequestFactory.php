91c3d09039ae73f680b4e2306477bdf3e225a43f
Dimitar Dimitrov <daghostman.dd@gmail.com>
2017-03-08 14:11:34 +0200

    Change to pass arguments to the server reqeust factory

--- src/Framework/Http/Factory/ServerRequestFactory.php
+++ src/Framework/Http/Factory/ServerRequestFactory.php
@@ -14,10 +14,24 @@ final class ServerRequestFactory implements FactoryInterface
      *
      * @param ContainerInterface $container DI Container
      *
-     * @return \Zend\Diactoros\ServerRequest
+     * @return \Psr\Http\Message\ServerRequestInterface
      */
     public function build(ContainerInterface $container): ServerRequestInterface
     {
-        return \Zend\Diactoros\ServerRequestFactory::fromGlobals();
+        $server = filter_input_array(INPUT_SERVER) ?? null;
+        $payload = null;
+        if (in_array($server['HTTP_METHOD'], ['POST', 'PUT', 'PATCH'], true) && isset($server['HTTP_CONTENT_TYPE'])) {
+            $payload = filter_input_array(INPUT_POST);
+            if ($server['HTTP_CONTENT_TYPE'] === 'application/json') {
+                $payload = json_decode(file_get_contents('php://input'), true);
+            }
+        }
+
+        return \Zend\Diactoros\ServerRequestFactory::fromGlobals(
+            $server,
+            filter_input_array(INPUT_GET) ?? null,
+            $payload,
+            filter_input_array(INPUT_COOKIE) ?? null
+        );
     }
 }
